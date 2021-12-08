b74e62b56fb96d8a26f89e459328f6d8674000af
Benjamin Tabor <bentabor6215@gmail.com>
2021-12-07 21:16:18 -0600

    Added routes for Portfolio, C2 sprint

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/learn-more', function(req, res, next) {
+    res.send({
+        "LongIntro":"I attended Merrill Highschool and graduated in 2017. After Highschool I attended UW-Marathon County and worked in multiple early childhood environments on my way to a bachelor’s degree in education. I wasn’t enjoying the education degree and decided it was time to switch majors. I decided to switch my major to Software Development and applied to NTC in December 2019. I am currently in my third semester at NTC and am on course to graduate in the Spring of 2022.",
+        "ShortIntro":"a software developer student"
+     });
+});
+
+router.get('/contact-form', function(req, res, next) {
+    res.send({
+        "FormTag":"<form class=\"contact-form\"><div class=\"form-title\">Contact Information</div><div class=\"form-body\"><div class=\"row\"><input class=\"first-name\" type=\"text\" placeholder=\"First Name*\"><input class=\"last-name\" type=\"text\" placeholder=\"Last Name*\"></div><div class=\"row\"><input class=\"phone\" type=\"tel\" placeholder=\"715 123-456*\"><input class=\"email\" type=\"email\" placeholder=\"Email Address*\"></div><div class=\"row\"><input class=\"comments\" type=\"text\" placeholder=\"Comments*\"></div></div><div class=\"rule\"></div><div class=\"form-footer\"><a type=\"submit\" class=\"formA submit\">Submit<span class=\"fa fa-thumbs-o-up\"></span></a><a class=\"formA cancel\">Cancel<span class=\"fa fa-ban\"></span></a></div></form>"
+     });
+});
+
 router.get('/form', function(req, res, next) {
   res.send({
     "tag": "<div class=\"card-form\" id=\"last-form\"><form class=\"zoo-form\" id=\"guest-group-from\"><div class=\"zoo-form-title\">Group Form</div><div class=\"zoo-form-body\"><div class=\"row\"><label class=\"label\" for=\"group-number\">Number of guests</label><input class=\"zooField\" type=\"number\" name=\"group-number\" id=\"group-number\"></div<div class=\"rule\"></div><div class=\"form-footer\"><button class=\"confrim-group-button\" type=\"button\">Admit Group</button></div></form></div>"
